bf2042940bd87f41b07ffb3c447b0cbf46bc1052
Wyundi <wyundi8615@outlook.com>
2023-04-05 13:58:54 -0400

    test for repo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,30 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+import google_maps_api_key from './config.js';
+
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+const center = {
+  lat: 40.7128,
+  lng: -74.0060
+};
+
+let html = (
+  <div>
+      <LoadScript googleMapsApiKey={google_maps_api_key}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+          <Marker position={center} />
+        </GoogleMap>
+      </LoadScript>
+    </div>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+  return html;
 }
 
 export default App;
---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+const google_maps_api_key = "AIzaSyBKF_-Lj9BeBzJ1SyMRq0b5qfgdZB3je9o";
+
+export default google_maps_api_key;
