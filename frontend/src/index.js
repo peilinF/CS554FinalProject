60a17146bd5acf5c798073a003d3c1e6ffadafdb
Leona <huangsylvia822@gmail.com>
2023-04-24 02:10:28 -0400

    Upload .env file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// Load environment variables from the .env file
+require('dotenv').config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
