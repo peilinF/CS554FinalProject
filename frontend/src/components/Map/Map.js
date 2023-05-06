e57135894b61fd2099d9b60275051c11a8f83527
Wyundi <wyundi8615@outlook.com>
2023-05-06 16:52:22 -0400

    avoid ferries

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -49,6 +49,9 @@ const Directions = ({ locations, handleDistance }) => {
                 waypoints: waypoints,
                 optimizeWaypoints: true,
                 travelMode: google.maps.TravelMode.WALKING,
+                avoidFerries: true,
+                avoidHighways: true,
+                avoidTolls: true,
             },
             (result, status) => {
                 if (status === google.maps.DirectionsStatus.OK) {
