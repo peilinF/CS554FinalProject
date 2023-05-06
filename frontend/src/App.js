aafee1e535742f31047dcf080cbebb6209704ef5
Wyundi <wyundi8615@outlook.com>
2023-05-06 06:38:24 -0400

    fix conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,11 @@ import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import ChatPage from "./pages/ChatPage";
 import ChangePassword from "./components/ChangePassword";
-import MyMapPage from "./pages/MyMapPage";
+
+// import MyMapPage from "./pages/MyMapPage";
+
+import MyMapPage from "./pages/Map";
+
 import HomePage from "./pages/HomePage";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase/firebase";
