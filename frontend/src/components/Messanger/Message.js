1c00d1f99b299fa44c5cfc33bbb26a35520064e2
MuradAles <muradaleskerov99@gmail.com>
2023-05-06 16:07:37 -0400

    new ui for chat

--- frontend/src/components/Messanger/Message.js
+++ frontend/src/components/Messanger/Message.js
@@ -30,34 +30,21 @@ const Message = ({ Text, Time, own }) => {
         const interval = setInterval(updateTimeAgo, 60000);
         return () => clearInterval(interval);
     }, [Time]);
+    //https://www.pluralsight.com/guides/applying-classes-conditionally-react
+    // The simplest approach is to use a ternary statement inside of a template literal.
     return (
         <div className="message">
-            {
-                own ? (
-
-                    <>
-                        <div className="ownText">{Text}</div>
-                        <div className="timeAgo">{timeAgo}</div>
-                    </>
-                ) : (
-                    <>
-                        <div className="frinedText">{Text}</div>
-                        <div className="timeAgo">{timeAgo}</div>
-                    </>
-                )
-            }
+            <div className={own ? "ownMessage" : "friendMessage"}>
+                <div className="messageContainer">
+                    <img className="conversationImg"
+                        src="https://occ-0-358-37.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZCI8XoAl54lPJXO4PqykYv3AqdO52_3chqk8wmDknaYfeo7PCFgzxG175QMEhMAMVZBJPLMDhiLS1fvEJFgTUtSJbGbF7iQZwQI.jpg?r=1dd"
+                        alt="Unknown" />
+                    <div className={own ? "ownText" : "friendText"}>{Text}</div>
+                </div>
+                <div className="timeAgo">{timeAgo}</div>
+            </div>
         </div>
     )
 };
 
 export default Message;
-
-{/* <div className={own ? "message own" : "message"}>
-<div className="messageTop">
-    <img className="conversationImg"
-        src="https://occ-0-358-37.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZCI8XoAl54lPJXO4PqykYv3AqdO52_3chqk8wmDknaYfeo7PCFgzxG175QMEhMAMVZBJPLMDhiLS1fvEJFgTUtSJbGbF7iQZwQI.jpg?r=1dd"
-        alt="Unknown" />
-    <div className="messageText">{Text}</div>
-</div>
-<div className="messageBot">{timeAgo}</div>
-</div> */}
