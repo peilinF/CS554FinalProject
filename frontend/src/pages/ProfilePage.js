c55adf2d9bfafffc8945a64cbfa195935f44266a
MuradAles <muradaleskerov99@gmail.com>
2023-05-07 16:27:06 -0400

    small changes in profile

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -14,41 +14,48 @@ const ProfilePage = () => {
     auth.currentUser.email ? auth.currentUser.email : ""
   );
 
-  const editProfile = () => {};
+  const editProfile = () => { };
   return (
     <MainLayout>
-      <div className="profile">
-        <h2>Profile</h2>
-        <div className="pimage">
-          {auth.currentUser.photoURL ? (
-            <img src={auth.currentUser.photoURL} />
-          ) : (
-            <AccountCircle id="a_icon" />
-          )}
+      <div className="container">
+        <div className="logBook">
+          <h2>logBook</h2>
         </div>
+        <div className="profile">
+          <h2>Profile</h2>
+          <div className="pimage">
+            {auth.currentUser.photoURL ? (
+              <img src={auth.currentUser.photoURL} />
+            ) : (
+              <AccountCircle id="a_icon" />
+            )}
+          </div>
 
-        <TextField
-          onChange={(e) => setName(e.target.value)}
-          style={{ margin: "10px", width: "70%" }}
-          value={name}
-          label="Name"
-        />
-        <TextField
-          style={{ margin: "10px", width: "70%" }}
-          value={email}
-          label="Email"
-          disabled={true}
-        />
+          <TextField
+            onChange={(e) => setName(e.target.value)}
+            style={{ margin: "10px", width: "70%" }}
+            value={name}
+            label="Name"
+          />
+          <TextField
+            style={{ margin: "10px", width: "70%" }}
+            value={email}
+            label="Email"
+            disabled={true}
+          />
+          <Button
+            onClick={editProfile}
+            disabled={auth.currentUser.displayName == name}
+            variant={"contained"}
+          >
+            Edit Profile
+          </Button>
 
-        <Button
-          onClick={editProfile}
-          disabled={auth.currentUser.displayName == name}
-          variant={"contained"}
-        >
-          Edit Profile
-        </Button>
-
-        <Button onClick={() => signOut(auth)}>Logout</Button>
+          <Button onClick={() => signOut(auth)}>Logout</Button>
+        </div>
+        <div className="friendList">
+          <h2>friendList</h2>
+        </div>
       </div>
     </MainLayout>
   );
