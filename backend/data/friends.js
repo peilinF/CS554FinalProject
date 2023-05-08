256cb40532265c276526263c966bc4fdefb59a79
MuradAles <muradaleskerov99@gmail.com>
2023-05-07 21:24:03 -0400

    small fix

--- backend/data/friends.js
+++ backend/data/friends.js
@@ -1,44 +1,157 @@
-import { conversations } from "../config/mongoCollections.js";
+import { abc, users } from "../config/mongoCollections.js";
+import { getUserById } from "./users.js";
 
-export const createConversation = async (UserId, FriendId) => {
-  const conversationCollection = await conversations();
-  const conversation = await conversationCollection.findOne({ $or: [{ members: [UserId, FriendId] }, { members: [FriendId, UserId] }] })
-  if (conversation !== null) throw 'Conversation already exists';
-  let newConversation = {
-    members: [UserId, FriendId],
-    showConversation: true
-  };
-  const newInsertInformation = await conversationCollection.insertOne(newConversation);
-  if (newInsertInformation.insertedCount === 0) throw 'Insert failed! (Conversation)';
-  return newConversation;
+export const getAllPeople = async (from) => {
+  const usersCollection = await users();
+
+  let people = await usersCollection.find({}).toArray();
+
+  let fromPerson = people.find((o) => o._id == from);
+
+  let res = [];
+
+  if (Array.isArray(people)) {
+    for (let i = 0; i < people.length; i++) {
+      let o = people[i];
+      delete o.email;
+      delete o.createdDate;
+      if (fromPerson.friendList.includes(o._id)) {
+        o["status"] = 1;
+      } else if (fromPerson.requests.includes(o._id)) {
+        o["status"] = 0;
+      } else o["status"] = -1;
+
+      res.push(o);
+    }
+  }
+
+  res = res.filter((o) => o._id !== from);
+
+  return res;
 };
 
-export const getAllConversationsByUserId = async (UserId) => {
-  const conversationCollection = await conversations();
-  const conversationsOfUser = await conversationCollection.find({ members: UserId, showConversation: true }).toArray();
-  console.log(conversationsOfUser)
-  if (conversationsOfUser.length === 0) return [];
-  return conversationsOfUser
+export const getRequests = async (uid) => {
+  const usersCollection = await users();
+  const data = await usersCollection.findOne({ _id: uid });
+
+  let requests = data.requests;
+  let result = [];
+
+  for (let i = 0; i < requests.length; i++) {
+    let udata = await getUserById(requests[i]);
+    if (udata !== null) {
+      result.push(udata);
+    }
+  }
+
+  return result;
 };
 
-export const deleteAllConversationsByUserId = async (UserId) => {
-  const conversationCollection = await conversations();
-  const deletionConversation = await conversationCollection.deleteMany({ members: UserId });
-  if (deletionConversation.deletedCount === 0) throw `Could not delete any conversations for user ${UserId}`;
-  return true;
+export const myFriends = async (uid) => {
+  const usersCollection = await users();
+  const user = await usersCollection.findOne({ _id: uid });
+  let flist = user.friendList;
+  let result = [];
+
+  for (let i = 0; i < flist.length; i++) {
+    let udata = await getUserById(flist[i]);
+    if (udata !== null) {
+      result.push(udata);
+    }
+  }
+
+  return result;
 };
 
-export const getConversationOfTwoUsersId = async (UserId, FriendId) => {
-  const conversationCollection = await conversations();
-  const conversation = await conversationCollection.findOne({ $or: [{ members: [UserId, FriendId] }, { members: [FriendId, UserId] }] });
-  console.log(conversation)
-  if (conversation === null) throw 'Conversations doesnts exists';
-  return conversation;
+export const addRequest = async (targetId, uid) => {
+  const userCollection = await users();
+  const doc = await userCollection.findOne({ _id: targetId });
+
+  let requests = doc.requests;
+  if (Array.isArray(requests)) {
+    if (requests.indexOf(uid) == -1) {
+      requests.push(uid);
+    }
+  }
+  const updateInfo = await userCollection.updateOne(
+    { _id: targetId },
+    { $set: { requests: requests } }
+  );
+
+  if (updateInfo.modifiedCount == 0) {
+    throw "Error";
+  }
+
+  return "Request Added";
 };
 
-export const deleteConversationOfTwoUsersId = async (UserId, FriendId) => {
-  const conversationCollection = await conversations();
-  const deletionConversation = await conversationCollection.deleteOne({ $or: [{ members: [UserId, FriendId] }, { members: [FriendId, UserId] }] });
-  if (deletionConversation.deletedCount === 0) throw `Could not delete any conversations for user ${UserId}`;
-  return true;
+export const acceptRequest = async (targetId, uid) => {
+  const usersCollection = await users();
+  const user = await usersCollection.findOne({ _id: targetId });
+  let requests = user.requests;
+  let friendList = user.friendList;
+  if (requests.includes(uid)) {
+    requests.splice(requests.indexOf(uid), 1);
+    friendList.push(uid);
+  } else throw "Request doesn't exist";
+
+  const updatedInfo = await usersCollection.updateOne(
+    { _id: targetId },
+    {
+      $set: {
+        requests: requests,
+        friendList: friendList,
+      },
+    }
+  );
+  if (updatedInfo.modifiedCount == 0) {
+    throw "Error Occurred";
+  }
+  return "Added";
 };
+
+export const removeFriend = async (targetId, uid) => {
+  const usersCollection = await users();
+  const user = await usersCollection.findOne({ _id: targetId });
+  let friendList = user.friendList;
+  if (friendList.includes(uid)) {
+    friendList.splice(friendList.indexOf(uid), 1);
+  } else throw "Friend doesn't exist";
+
+  const updatedInfo = await usersCollection.updateOne(
+    { _id: targetId },
+    {
+      $set: {
+        friendList: friendList,
+      },
+    }
+  );
+  if (updatedInfo.modifiedCount == 0) {
+    throw "Error Occurred";
+  }
+  return "Removed";
+};
+
+export const declineRequest = async (targetId, uid) => {
+  const userCollection = await users();
+  const doc = await userCollection.findOne({ _id: targetId });
+
+  let requests = doc.requests;
+  if (Array.isArray(requests)) {
+    const index = requests.indexOf(uid);
+    if (index !== -1) {
+      requests.splice(index, 1);
+    }
+  }
+  const updateInfo = await userCollection.updateOne(
+    { _id: targetId },
+    { $set: { requests: requests } }
+  );
+
+  if (updateInfo.modifiedCount === 0) {
+    throw "Error";
+  }
+
+  return "Request Removed";
+};
+
